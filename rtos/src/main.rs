9677160f603aadb85e9ebbbbe28d17f781fdbce3
YdrMaster <ydrml@hotmail.com>
2022-10-24 16:25:43 +0800

    init
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

---
+++ rtos/src/main.rs
@@ -0,0 +1,42 @@
+#![no_std]
+#![no_main]
+#![feature(naked_functions, asm_const)]
+#![deny(warnings)]
+
+/// Supervisor 汇编入口。
+///
+/// 设置栈并跳转到 Rust。
+#[naked]
+#[no_mangle]
+#[link_section = ".text.entry"]
+unsafe extern "C" fn _start() -> ! {
+    const STACK_SIZE: usize = 4096;
+
+    #[link_section = ".bss.uninit"]
+    static mut STACK: [u8; STACK_SIZE] = [0u8; STACK_SIZE];
+
+    core::arch::asm!(
+        "la sp, {stack} + {stack_size}",
+        "j  {main}",
+        stack_size = const STACK_SIZE,
+        stack      =   sym STACK,
+        main       =   sym rust_main,
+        options(noreturn),
+    )
+}
+
+/// 非常简单的 Supervisor 裸机程序。
+///
+/// 打印 `Hello, World!`，然后关机。
+extern "C" fn rust_main() -> ! {
+    for c in b"Hello, world!\n" {
+        unsafe { (0x1000_0000 as *mut usize).write_volatile(*c as _) };
+    }
+    unreachable!()
+}
+
+/// Rust 异常处理函数，以异常方式关机。
+#[panic_handler]
+fn panic(_: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
